2794c737ed5880f59fbf3232330817d13b47f55f
Brandon Laubaugh <kingkazma133@gmail.com>
2021-07-08 20:06:38 -0400

    fixed it cause my friends are cool

--- server.js
+++ server.js
@@ -2,17 +2,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/', function(req, res) {
-    res.sendFile( path.join(__dirname, 'Home.HTML') );
-})
+// app.get('/', function(req, res) {
+//     res.sendFile( path.join(__dirname, 'Home.HTML') );
+// })
 
-app.get('/styles', function(req, res) {
-    res.sendFile( path.join(__dirname, 'style.css') );
-})
+// app.get('/styles', function(req, res) {
+//     res.sendFile( path.join(__dirname, 'style.css') );
+// })
 
-app.get('/js', function(req, res) {
-    res.sendFile( path.join(__dirname, 'server.js') )
+// app.get('/js', function(req, res) {
+//     res.sendFile( path.join(__dirname, 'server.js') )
+// })
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, "/public/Home.html"));
+
 })
+app.use(express.static("public"));
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
